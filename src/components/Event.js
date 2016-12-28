fdb90a14651efd2de4430387869da1cfa6488baf
Miguel Costa <maiquedi@gmail.com>
2016-12-28 08:58:17 +0000

    update Event object with new methods for getting data and fixing errors related to optional parameters and a bug on setDate

--- src/components/Event.js
+++ src/components/Event.js
@@ -13,21 +13,25 @@ function getCurrentDay() {
   return date.getDate();
 }
 
+/**
+ *  An event class for creating event objects that include a day, month, year, title, description and optional picture and isYearly
+ *  In case an event is yearly, it returns 'all' in the getYear method
+ */
 export default class Event {
   // WARNING: month is in the 1-12 format, not the default 0-11 one
-  constructor({day=getCurrentDay(), month=getCurrentMonth(), year=getCurrentYear(), title="No title", description="No description", picture, isYearly=false} ) {
+  constructor({day=getCurrentDay(), month=getCurrentMonth(), year=getCurrentYear(), title="", description="", picture, isYearly=false} = {} ) {
 
     this.date = new Date();
-    const date = this.date;
 
     // set year to current if it is 'all'... for creating a real date, nothing else
     if (typeof year !== 'number') {
       year = getCurrentYear();
     }
-
-    date.setDate(day);
-    date.setMonth(parseInt(month) - 1);
-    date.setYear(year);
+    
+    // I guess the day should come last, for it may change everything else?
+    this.date.setYear(year);
+    this.date.setMonth(parseInt(month) - 1);
+    this.date.setDate(day);
 
     this.isYearly = isYearly;
     this.title = title;
@@ -42,5 +46,46 @@ export default class Event {
   }
   getDay() {
     return this.date.getDate();
+  }
+  getDescription() {
+    return this.description;
+  }
+  getTitle() {
+    return this.title;
+  }
+  getPicture() {
+    return this.picture;
+  }
+  setPicture(pictureUrl) {
+    this.picture = pictureUrl;
+  }
+  setDescription(text) {
+    this.description = text;
+  }
+  setTitle(text) {
+    this.title = text;
+  }
+  setYear(year) {
+    this.date.setYear(year);
+  }
+  setMonth(month) {
+    this.date.setMonth(month - 1);
+  }
+  setDay(day) {
+    this.date.setDate(day);
+  }
+  setDate(dateOrObject = {day, month, year}) {
+    if (dateOrObject instanceof Date) {
+      this.date = dateOrObject;
+    } else {
+      let {day, month, year} = dateOrObject;
+      
+      year && this.setYear(year);
+      month && this.setMonth(month);
+      day && this.setDay(day);
+    }
+  }
+  isYearly() {
+    return this.isYearly;
   }
 }
