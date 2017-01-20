34d4bd4c5309efe7c243b971880c00d588edb1c1
Painatalman <maiquedi@gmail.com>
2017-01-20 20:33:03 +0000

    add compiled demo and distributed app

--- demo/demo.js
+++ demo/demo.js
@@ -76,7 +76,25 @@
 	  title: "Anos da Celina"
 	}];
 	
-	var calendar = new _Calendar2.default("#app", { year: 2016, month: 4, pictureUrl: 'https://lh3.googleusercontent.com/A798ePpnjRpz5TUnrmxMYOddTo9p172jh1S03cwaEWwV3QSz-rNveC-D2G4qlGhKFXTP2_sTdWdlWkc7CdvAWFHnPAkyJUcdJ9bQvYzsxkGym3VPMiEpaH4jPVsQDzsRHsPESW8bD6-0SVExtl2TfKtekE789WELSdD31XrNgYX4p-jrft4RIkrx-zeUHogDFKb3OYOXEUxWb8rAUmU6u4mNi5JV1Te9_muN2p-LcNmnO1LH7Anr_vYcjuyr8KKPeEaNvik7MQthIPhy7idj0iXA4y4AATLSToZvCztuMedFmoui5HyTL141SvqLWA5cw9i8v74t5oYPQNCgcNNVpmNQ-4mS0gzGnm0-3sn6SkmW2_tT-QSbv5gK7S9iv9GWfh0UKquymwrQYmxeQwf4_J-aTtaItYyqcvZpBr5xQ1vM2HyLFFEmgeJVJZn3RjAWqhiCeVo8zyUEBFt2cO_B7hUouPnSWJN2Qfvgmi8Idh50-odWXaUdiHkWaU9T49BP1Jnm-zRbUZVtLij4ZnXYijYW3bdRZi5BI7TZOR88vZt0nPMUnVXV19YEu3XCAmI9hbZt=w1010-h606-no', eventSettingObjects: eventSettingObjects });
+	var pictureUrls = {
+	  'default': 'https://lh3.googleusercontent.com/A798ePpnjRpz5TUnrmxMYOddTo9p172jh1S03cwaEWwV3QSz-rNveC-D2G4qlGhKFXTP2_sTdWdlWkc7CdvAWFHnPAkyJUcdJ9bQvYzsxkGym3VPMiEpaH4jPVsQDzsRHsPESW8bD6-0SVExtl2TfKtekE789WELSdD31XrNgYX4p-jrft4RIkrx-zeUHogDFKb3OYOXEUxWb8rAUmU6u4mNi5JV1Te9_muN2p-LcNmnO1LH7Anr_vYcjuyr8KKPeEaNvik7MQthIPhy7idj0iXA4y4AATLSToZvCztuMedFmoui5HyTL141SvqLWA5cw9i8v74t5oYPQNCgcNNVpmNQ-4mS0gzGnm0-3sn6SkmW2_tT-QSbv5gK7S9iv9GWfh0UKquymwrQYmxeQwf4_J-aTtaItYyqcvZpBr5xQ1vM2HyLFFEmgeJVJZn3RjAWqhiCeVo8zyUEBFt2cO_B7hUouPnSWJN2Qfvgmi8Idh50-odWXaUdiHkWaU9T49BP1Jnm-zRbUZVtLij4ZnXYijYW3bdRZi5BI7TZOR88vZt0nPMUnVXV19YEu3XCAmI9hbZt=w1010-h606-no',
+	  'all': {
+	    10: 'https://lh3.googleusercontent.com/7-nWkiuilgTMOyKxvHTYYmI_G3LKzFKn5pqswUOgmZ-_DMoqtpiQJRyvOetJyp6JhrZv5r1MWF9UA8SVBEealws8PQ7XUNc46sw_9O80ftKl3ty4XpAj8BOmAUcb-y900c1XZPV0kB7g29YbmfvXNKk7FCdMLnQStwowQmmoFUdNwvs27xkI1UkpTvinG3oLXsCYGKbMsSjJCgM6dR9m34VLVAjQiaDnI-_Uj0KOW9hwrZTmsj7nXfiB5JD-ya_vWSEMb4slqLS7QqHS40yvbIA7E3q5D8P-u4RIbL_p43ofIeJrAt2doldB25Q7zbxJgOnxqJOi89ns7J88bLf0RxzxxZCuNRYFqmb9pceXYuJXtWEEhPTYAzuXW4HbMJA9ni62SG-AmF1gk_nq5KrzGJ1kJfvIEqGWYsb6usgE-PMKRVo22HAqPnMdnaHA4hJPaMDyenlVq51Hti_274f59UAqEF2BrGljUOBWzKSeEfoVrp3Vbp2X4z57pjs5A-f0vm6mvHJ2geCvklwX1NV2iK2cgbg4yhnL2RJlSnrLmfHvWtEpNh3qAYSsbmY3lW7QNR-Z8WuUKOV0IJzDWoM_aohfyarYVIQrkolQajI8PagquWVi=w356-h201-no',
+	    8: 'https://lh3.googleusercontent.com/BNQYn7fyryotN1kah9eSQyfnF4oPjeVNsGCNNIc91ONsfk60b5aY7qw9smnBtYR-zYCS3nRg3TKNEkzlYBaESKVkOoyZ92gzisXqx0t49hhmajIV3QuImZRA8kH2ehuNC_UuhMqPVR1pvNdG--y4Kn8ZtBE1gR5JPFPd5GALtxYGnijFIZ-PFjlM0VecmdW8YZ9nWpioQppR3FakcthuEpiTvvED_e7hosvhUCgDUGM_itwaccaTK2osTeRGINvLLTX2oMZZlFrTM6fMBzc68jAqf0efp-RUB_WFIfKfH83IEtxa2cevszJcIh9Hf3Kjn0KRFqmQ-1_0SsKEI3wfGRZHRbgAyoYvhGg8GcxP8T-UVjCpBnyd9t_004AVcosv-o60-YigbkhW7NKebfr8y-udBZrwC3JJwNI97FIfJxP_nwJKqiZgbdXwJKpRADac-suoUpJB8Ti425zs2ZIadqtYITC0MdhDD18iekgVVynhCTFIgCFY8gHBKdji0LVlBRbJklcs2U_YjDe_P9ttVVCsmMiLLw3M99LjdMIIaD7Ak8bNHrVnhuci-Z5-hi6dFfY1reCZaoiRSLJTtgyi5zMJ9TEA8VJ4v7fFnTUPDbPaskEd=w298-h179-no',
+	    4: 'https://lh3.googleusercontent.com/_ywMQV-vCNC6CTP59ZUjfpJeQ82_9yKypBdTroxGDZfaQCIedXsulmV_o4lfjPHPOPR3Gg2cI8AIQWF493ZS0hQ-ODAxnUvP2gZ7S3h5oMbbqOXwPSBBa9YnXpwIYVucvlKl5ddNxpe9c23xXPcEJ3ogKAS9Gj0arhq50v1lLw0Qgmysq0KvsP-H27nw6Yp7Xq-rv86BStMyoUHpSbRb7VXhgdq4vcXBOrXMwzFmJ45kj172NRMCgkWs8Cz_IDN9ODPeD3Rhhbp3MBqo_LrxfmUZ5y5W_e9vX7rO_QVskDRp7q9mRlvrxiG2LeyMxe_O6pL6-OY_rh0nBanLNSglA9hR_eA8VymKxRAX27jh8M1dEgpdfmHmHlQ6sK_XuOoI_jLfTQtKcbXHteCpaDtSfoFq5bNuX3ekBEb_VO14p6fISDyXCafu-dTne_ftVAq4bSNsVRuWzEkbQY0lVbZYqdhfA6bMKqWmryJPrrSm_9hTNfurY6k3ckagYWDWVltYHjP5b_HWGGi9oUHa4PTOeCc8N2tyDozgUmrkzK1zFzuuXa_J3jjIJg_BJEwrQIjW0AwtuHKxqcdelCq11TkyptWotLsoUADfY1SDRfQAAJ4hsLhG=w375-h282-no',
+	    2: 'https://lh3.googleusercontent.com/aghgSa8NnBOJiM0RZZT2N5wSUOYrQb4VWeIJa941CoSbzdy-QZGP9-PHxeU5_qBthTQN6OOm3I6whvgtA-HPGI-JaZrePO6xSIJ9gTyysQhGwi3sw4WAuTGib8W_r_eCI5QTk9ROlIQuUkc79CiGUifph4tv2QV2Te1_L9XP6cxQnH-QHbOI0qBCflaf7mj8e-wkbkAMLVVFIe1KYbslpahKwqRCD3DZRw2sK6SfcYc5apdWEvSHLoAUhkalry-UcETxkEjLosMqKZzNnTvUdoMFGiel6U20jnjLVgOUP8HLk_1GYqZHL4bQ6Iy5wXZRr9gqcbvs7h0jFEcjJD7A-FM4uCjqvRJH9o9SZV6gqTlAORD9Y6CvuunzcmIsrtQ2ANhuV3HQ6ASpx8wgo78dXv4NrTtRYkLu62IUI9gWCHViLpXYZjr2N0VSAYIDR21af6IMJLpJ6l9CXrik4M68aG_6aZZHcbtW9ut_Zc1G-_bbavAu7Ll29V2_R-EBfIrjyMH4NV9gmX0ODFDWj-0vLm42SkxObdPkNExx7VtytXZoF1gFw--fphc7dSxJmZRxuINYss5R9GSCRits-koqP1-T5S6FCplKhjMXFSe5FvVKeILX=w355-h200-no',
+	    6: 'https://lh3.googleusercontent.com/0FVikr14_EJVvL8eT_GmyGifkSgBMDIKkejfwsVO-NJgvYVbN5_lPzu1HPUbqVbJu20wGKrEvLYq9o1oaieizJw38nJxoxZXqDHGe0WYaXae3bwXs7EpNoofrQ2TJGwnhHtrRZqhp1dfI811WyzUSNpDKOJHCNdEFSz60FR45DHPQetEA66ItDl6EtjmyrDlH6wg3V1TQVmvVFIVjA9q6UwcojHUTPvlvYYV3mCJMBfP0LXVL93lJAMV545iYkiPxPRm8k9YQ1tJVj85mv7s1Gt4-OYjW4FGeWnINSwDWQpI2GXAR0I8Nz12lgcUBQ1prx1PSa8G7bCEef9u5K-5fFGPWiMZTl7rzpg9IvA8H0GeOePYs5RUbfS9JO2HlCyjqJphx2k4FRtVA-uyC-E2hJHHrXe5jWmh-4sJvbwJ8qJHsJeNaW4nGt2VYHc8oo14cEXsnGPOiJcnO0aGH8GCTw5PA-qe4Hb2aRv5Q15C67kEGLP6fNeTj0fBoQnhXk59NS_Gs3wpfwbX-x9zoeG2t1ZgHaEaen8nr_ew73Zx3vqM7PbXGYMGrmtfr9d8tV-I83BArd8jHx6hMnyDaxGV18-lZVo4AfG665ZDfsYQhCmXkwSe=w356-h267-no',
+	    9: 'https://lh3.googleusercontent.com/KKIyri2N-tgJO4VmAIvq8rJsD5LcDBrHL6AP4VLe5hlcsTBC7Cth1IJUB53gRrX-crRrf545gxiy3WOus5it-YR6KoHn_-n9zXfiuBl1u5byWXk6613wZjHgwH3DRlM_jcLGVfXIP_9inMtoBZ0oAKfLlnOWK5ZtEFOtujyChJ1iPIKiUlDa1L7rjXPjwCkZindr9-IX26yKgMlJsxwz1Y__r3a_M6fdwuD-QwKeW8E6BbxOWS29yPle6dy8yJczsRw0CcLxuUCGOdOWbIoSTkE3wW9fpDiPL5NnZHXq-VApEacchYDpeUzfu9zrtjvzZyYHaUA2yjLwPCIWnXv_Xrky_HxyooojVej41Q3KyaLUEWgnXtXQJHhOrO2ppRxD3y4y6aPGjHpEm9pS-Kd8jwasLROTUwfKxbDwoLRqSaZsNhub_WfpS6lxAaF4x-uu04V27WT_rsVvf407NgRXHp2r1wrYyuJN0ooSQXWZzdFMpkLN3ptOEt6rKYcqTzpCcP2VpI8smri05oJLot2_aNeZIzL1zk4jx5PhSPLUP4WTZe8_QYR9F6H-Dl_z5f3bTkh0QeTRd1F2PbEWBuX-9JAOzeSw-j8pkPS2qMEcW5mJzvCR=w803-h452-no',
+	    11: 'https://lh3.googleusercontent.com/Siw_SQ97ZSFzN2_4JFPOpsVfmiNdWtEf6Sq7eFb2SO-gBmBpngDki8FC1X4ohDgBlzuPhjUxROqmWaKh0t01ucsZeLCf_DVqzDurV8XoGrFUra-s26NDP2Gb2YuEMWTcKZIdjgQ_KrcqataPUn-TjoVrhiQdvmsGOZMHBbaKbpVcHflhWjUvW8G1UAIjPkRaayOkBYe1S1huKBEC5FAKFhSZvD2YMCR1QIfQY9KvhSB-1W5DjcoacIHF0I_FBAwoL9IxHUg4UdIeYgTJuFnQ5jqhRsLcKP1uE__DWaJxICwHoVQAQNBhUbZPTlHbNqzftrMj8eE8pee4y2R4Q-as7nRwGNz_GcR8X08nZlsw7i1VtyhiJU9Exq31ITmtca8ylUqMyVIUInMiVF43Sb8M_8naELjoQy_qOAd5QK1a7yPc1PPp8CbnAwlO4-iG2K73evhfwx88H8cbj4sXG3Yzq3WAedjbqOx1wj0uOpUWk0cUWyFnGAwDtiQVpbRKKdUxb2laVooSe_2d5aK3dBp1GiXNsCSAQ7R0utKTcVvLCTACHhBxpR-P_msx3dIjRvDDpP4ST0zOzdjX_aAXstDZ1hXU9fbD5tkVmVnQQDNJd91Ch2oi=w443-h249-no'
+	  },
+	  2017: {
+	    1: 'https://lh3.googleusercontent.com/m-pSX--s9aTsNxZYezjTVtgHSIbLu0JB_aaPnhGD_Z0QcTYvOh2ifLhMW4lzXszh557fSmmZWPxzBH4-yup6SHlCCmH8-Aa4jWtczjeAG9rfqd6EEWbQJNfLOA49yKgoIljyitVjlmO-X-WlzSTOFKLj6tvZFIcxDefASWbWtLgGbAXOoIQ4cO0J9mvl18czWGh4cPkOsQLNg0e00CwurPB6NRAYM5UoxjN2i1yH4j8cnBcCMwVKs55E1UkYjTDMTQ7ZCit2qswxtX-dK7R3njnkyPCVH8s_8Y5NJs1ruvdmaqXsc1gjznnQ9Y6pGDmyHPlDdSOmxr86qlFUscFIRTvGH0x0xOdEh4psuovmWuIXHyDHCjkV1oO6pU60BRFgIulv_O0ymZHNia9m_mHdLfjaujTalXjikiDpE6SAavknAh6K2OOPDHZA7S52vrAUT4U_9RJwiCMl54z3tEVkaBEpfpBfSb1vB3otBjrk9CoWb7WkN33WsCEoRpwERpzLenwWg7b6bwsR3KWpWXukUQItVEPMN6x87Gfl_lyR9N-1_HSlAyb5QhBxEhG_UyTUhqZaUa-HyPR8BCSvK3bgSSIZ1WchGoGRLZaHYe9jwKKon7OZ=w116-h206-no',
+	    6: 'https://lh3.googleusercontent.com/9ZdGN5Of7qOhIJ1GrYmnpdsPuSM6vlqZP_yIE7KnPpablnEkJzhdixbm3XSloPMNozTOV5cWo46WIkXcbqogHYRm5Qp1h4ySsdkvCA38TAbgNrGInnWNZFniI7guCGeV4b1p0Wfxj-LacvpFCn4pg0QzferREVUfb-xM3F9McsrnGHNJVlB_K8Q_XC_vduQZGYqNaMKIcxrV1yW_A34VDSD26jcXpQouuqSt1I1S7rjg1Krl_aHVzj5ocE2VHJJUo4SuOXqfZOrczY-87ldNdt49avX3Qkecdm2mRY_LfnCgWzu1-_RpxwH6v6qkguyU1xVDFkFImvkQ02IBW-0F6LuYUAnD7fyOndlbDQ9QGTF98lfPNEj4TA0NU2IGXRihN6KlBFdLbAYs81p-937y_wLBu35GzKuhrNFQFEtGr0OiW7ZLxA-ofF7fThoQK8t7mr90546-Te3j9C7LooMSVOeNRC4a3Gf52qU9lUbDHXLNMw08y2b55fSYk3MojCJg7uCcKh-B8K8Azo9LeZ5Od5FPg-bHU_76Lsx3Pvp0Hq_d3TQfFhb8NUtOt7RTlsi6pWbqWAWKK6C3nNRRBbz1-ono2jb3Mnl_FlZn16IWiZQxcSll=w763-h430-no',
+	    10: 'https://lh3.googleusercontent.com/43vVeCDi3SzzoPl1qF1E1E2FPX_msL5E5dd-uh4EGUX9KBgZ69PhOEMYfe5Mc1YKO8F4ovSpzfe-HEM-ZenR05zhb19_-Mhp7SBryl7AISGcOGgNaUD83b-yWYpVFL7n1iTN1jH5xqzUIRoZGHWtPrSnJpzRKvfDSjexiAiWGF7oLxkaJRRCF5iFQk7iHK-uISuUKRjEwyvc6CBgFp42EMjp47dITpQwLxRssRp8PFk9kV5kWw_84SsqcorKDbJikX4kmFQJ9gZVsWHBOg5gGPv7cViIaDYTe2vBiOx6y91wSdOc1_U6ToYxN_VU9iTOjgu2QyGMc3_YydY9KSyWvB1-6-gcJR3G3bkPpTXcvrxVZ4Rv5JcAEDv1emK2oZ2uuisTBNBnx0ZERkgLxcZEeQ5vlSjb1I5ZkTxBq8bij80bpsIQEo0efFB-Ly5RM0X0ztpdgAWeCjxix-nsXIA9KlyYBvdO9bnbEySLemo5rLVyPNElurCQZYce_WmrB9cl25lMUbUC-yG2OWZsltXQlY71hS0V9F97D-8t57rQAiQ0dqTDj87btKn6tyQchHkOXu0Auj9KadYjAsHhojRsyuroJHrEK5A2Z5L8pOFcQLpll8vB=w355-h200-no'
+	  }
+	};
+	
+	var calendar = new _Calendar2.default("#app", { year: 2016, month: 4, pictureUrls: pictureUrls, eventSettingObjects: eventSettingObjects });
 	
 	calendar.createAndAddEvent({
 	  day: 25,
@@ -178,6 +196,7 @@
 	    var _ref$multipleElements = _ref.multipleElements;
 	    var multipleElements = _ref$multipleElements === undefined ? false : _ref$multipleElements;
 	    var pictureUrl = _ref.pictureUrl;
+	    var pictureUrls = _ref.pictureUrls;
 	    var _ref$eventSettingObje = _ref.eventSettingObjects;
 	    var eventSettingObjects = _ref$eventSettingObje === undefined ? [] : _ref$eventSettingObje;
 	
@@ -194,6 +213,7 @@
 	    this.isRendered = false;
 	
 	    this.pictureUrl = pictureUrl;
+	    this.pictureUrls = pictureUrls;
 	    // TODO: set for multiple elements
 	    this.element = document.querySelector(elementSelector);
 	    // TODO: set data attributes
@@ -294,12 +314,13 @@
 	
 	      var headerNode = document.createElement("header");
 	      var headerPictureFrameNode = document.createElement("div");
-	      var headerPictureNode = document.createElement("img");
+	      // let headerPictureNode = document.createElement("img");
 	      var headerTitleNode = document.createElement("div");
 	
 	      var prevNavNode = this.getCalendarNavNode(false);
 	      var nextNavNode = this.getCalendarNavNode(true);
 	      var titleNode = this.getCalendarTitleNode();
+	      var pictureUrl = this.getCalendarImageSource();
 	
 	      headerNode.classList.add(_Calendar2.default["calendar-widget__header"]);
 	      headerTitleNode.appendChild(prevNavNode);
@@ -309,12 +330,12 @@
 	      headerTitleNode.classList.add(_Calendar2.default["calendar-widget__title"]);
 	
 	      // picture-specific
-	      if (this.pictureUrl) {
+	      if (pictureUrl) {
 	        headerPictureFrameNode.classList.add(_Calendar2.default["calendar-widget__picture-frame"]);
-	        headerPictureNode.classList.add(_Calendar2.default["calendar-widget__picture"]);
-	        headerPictureNode.setAttribute("src", this.pictureUrl);
+	        // headerPictureNode.classList.add(styles["calendar-widget__picture"]);
+	        headerPictureFrameNode.style.backgroundImage = "url(\"" + pictureUrl + "\")";
 	
-	        headerPictureFrameNode.appendChild(headerPictureNode);
+	        // headerPictureFrameNode.appendChild(headerPictureNode);
 	        headerNode.appendChild(headerPictureFrameNode);
 	      }
 	
@@ -339,6 +360,31 @@
 	      });
 	
 	      return titleSpan;
+	    }
+	    /**
+	     * Gets the calendar image source.
+	     * First, it checks for a this.pictureUrls object with either an attribute corresponding to the current year and month
+	     * If it does not exist, it checks for an 'all' and current month image url
+	     * Finally, it returns a default this.pictureUrl or this.pictureUrls.default
+	     */
+	
+	  }, {
+	    key: "getCalendarImageSource",
+	    value: function getCalendarImageSource() {
+	      var imageUrl = void 0;
+	      var pictureUrls = this.pictureUrls;
+	      var currentMonth = this.curDate.getMonth() + 1;
+	      var currentYear = this.curDate.getFullYear();
+	
+	      if (pictureUrls && pictureUrls[currentYear] && pictureUrls[currentYear][currentMonth]) {
+	        imageUrl = this.pictureUrls[currentYear][currentMonth];
+	      } else if (pictureUrls && pictureUrls['all'] && pictureUrls['all'][currentMonth]) {
+	        imageUrl = pictureUrls['all'][currentMonth];
+	      } else {
+	        imageUrl = pictureUrls && pictureUrls['default'] || this.pictureUrl;
+	      }
+	
+	      return imageUrl;
 	    }
 	    /**
 	     * Gets a calendar navigation node, which can be used to either go to the next or previous month.
@@ -758,6 +804,32 @@
 	
 	      return singleEvents.concat(yearlyEvents);
 	    }
+	  }, {
+	    key: "fetchEventsFromGoogleCalendar",
+	    value: function fetchEventsFromGoogleCalendar(calendarId, apiKey) {
+	      var baseUrl = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'https://www.googleapis.com/calendar/v3/calendars';
+	
+	      var url = baseUrl + "/" + calendarId + "/" + apiKey;
+	
+	      var request = new XMLHttpRequest();
+	      request.open('GET', url, true);
+	
+	      request.onload = function () {
+	        if (request.status >= 200 && request.status < 400) {
+	          // Success!
+	          var resp = request.responseText;
+	        } else {
+	          // We reached our target server, but it returned an error
+	
+	        }
+	      };
+	
+	      request.onerror = function () {
+	        // There was a connection error of some sort
+	      };
+	
+	      request.send();
+	    }
 	  }]);
 	
 	  return EventCollection;
@@ -1004,7 +1076,7 @@
 	
 	
 	// module
-	exports.push([module.id, "/**\n@use postcss-nested;\n**/\n\n\n* {\n  box-sizing: border-box;\n  padding: 0;\n  margin: 0;\n}\n\n._20I9VEEji0s14PwqBp3tBd {\n  position: relative;\n\n  display: inline-block;\n\n  width: 100%;\n  min-width: 240px;\n  max-width: 450px;\n\n  border: 1px solid transparent;\n\n  box-shadow: 2px 2px 2px black;\n}\n\n._24lt64jWBF7GU3wrTEsjlO {\n\n}\n\n.iZ5NERKXuDEBNZf4YWTWR {\n  width: 100%;\n  min-height: 100%;\n}\n._2yaNxRA_OKEWfQ4QJHWOEL {\n  position: relative;\n  width: 100%;\n  height: 240px;\n  overflow: hidden;\n}\n._2HigPtClDF1kAFkKvX0Tes {\n text-align: center;\n  font-size: 1.3em;\n  padding: 12px 0;\n  margin-bottom: 12px;\n  box-shadow: 0 2px 5px black;\n  text-transform: uppercase;\n  font-weight: bold;\n}\n._2a3hDQbJCUej8fdLCnIl3u {\n  text-align: center;\n  width: 100%;\n}\n._1Kx9i6nPLghy3kISjsUO4y {\n\n  height: 35px;\n  cursor: pointer;\n}\n._1mL5_0d8xEPxuWSNvTw-WC {\n  color: salmon;\n  font-weight: bold;\n  cursor: pointer;\n  text-decoration: underline;\n}\n.TEpu29FpFto3Y_gMU92uF {\n  color: lightgray;\n}\n\n._2tSmy4npf6HLp4pP0MlFXP {\n  padding: 0 32px;\n\n  cursor: pointer;\n}\n\n._1yQg1VhhWUU_lnggq6Lo_s {\n  float: left;\n}\n\n._1yQg1VhhWUU_lnggq6Lo_s::before {\n  content:\"\\25C3\";\n}\n\n.LndY2V7viDXupPQS2K9JP {\n  float: right;\n}\n\n.LndY2V7viDXupPQS2K9JP::before {\n  content:\"\\25B6\";\n}\n\n._2xqMR2rBCDqE9wa71p_v5K {\n  position: absolute;\n  top: 0;\n\n  width: 100%;\n  height: 240px;\n  padding: 12px;\n  overflow: hidden;\n\n  background: white;\n\n  transition: height 250ms ease-in-out, padding 250ms ease-in-out 250ms;\n}\n\n.syNTZLOTwsRCWQID7n5Xm {\n  font-size: 16px;\n  width: 80%;\n}\n\n._1uNIq5kC5PmuF1ezvFZBaa {\n  width: 20%;\n}\n\n._2ofXAMY1YJNe-MwDI72LOa {\n  width: auto;\n}\n\n._3CPfGdbOlSRz-7DHRD7RAQ {\n\n}\n\n._1434u6FLprJrrACO_QmwoY {\n  margin-bottom: 6px;\n}\n\n._3bTa6V-vUZCTFX4vsSmMIY {\n  height: 0;\n  padding: 0;\n}\n", ""]);
+	exports.push([module.id, "/**\n@use postcss-nested;\n**/\n\n\n* {\n  box-sizing: border-box;\n  padding: 0;\n  margin: 0;\n}\n\n._20I9VEEji0s14PwqBp3tBd {\n  position: relative;\n\n  display: inline-block;\n\n  width: 100%;\n  min-width: 240px;\n  max-width: 450px;\n\n  border: 1px solid transparent;\n\n  box-shadow: 2px 2px 2px black;\n}\n\n._24lt64jWBF7GU3wrTEsjlO {\n\n}\n\n.iZ5NERKXuDEBNZf4YWTWR {\n  width: 100%;\n  \n}\n._2yaNxRA_OKEWfQ4QJHWOEL {\n  position: relative;\n  width: 100%;\n  padding-bottom: 56.25%; /* 16:9 aspect ratio*/\n  background-position: center center;\n  background-size: cover;\n}\n._2HigPtClDF1kAFkKvX0Tes {\n text-align: center;\n  font-size: 1.3em;\n  padding: 12px 0;\n  margin-bottom: 12px;\n  box-shadow: 0 2px 5px black;\n  text-transform: uppercase;\n  font-weight: bold;\n}\n._2a3hDQbJCUej8fdLCnIl3u {\n  text-align: center;\n  width: 100%;\n}\n._1Kx9i6nPLghy3kISjsUO4y {\n\n  height: 35px;\n  cursor: pointer;\n}\n._1mL5_0d8xEPxuWSNvTw-WC {\n  color: salmon;\n  font-weight: bold;\n  cursor: pointer;\n  text-decoration: underline;\n}\n.TEpu29FpFto3Y_gMU92uF {\n  color: lightgray;\n}\n\n._2tSmy4npf6HLp4pP0MlFXP {\n  padding: 0 32px;\n\n  cursor: pointer;\n}\n\n._1yQg1VhhWUU_lnggq6Lo_s {\n  float: left;\n}\n\n._1yQg1VhhWUU_lnggq6Lo_s::before {\n  content:\"\\25C3\";\n}\n\n.LndY2V7viDXupPQS2K9JP {\n  float: right;\n}\n\n.LndY2V7viDXupPQS2K9JP::before {\n  content:\"\\25B6\";\n}\n\n._2xqMR2rBCDqE9wa71p_v5K {\n  position: absolute;\n  top: 0;\n\n  width: 100%;\n  height: 240px;\n  padding: 12px;\n  overflow: hidden;\n\n  background: white;\n\n  transition: height 250ms ease-in-out, padding 250ms ease-in-out 250ms;\n}\n\n.syNTZLOTwsRCWQID7n5Xm {\n  font-size: 16px;\n  width: 80%;\n}\n\n._1uNIq5kC5PmuF1ezvFZBaa {\n  width: 20%;\n}\n\n._2ofXAMY1YJNe-MwDI72LOa {\n  width: auto;\n}\n\n._3CPfGdbOlSRz-7DHRD7RAQ {\n\n}\n\n._1434u6FLprJrrACO_QmwoY {\n  margin-bottom: 6px;\n}\n\n._3bTa6V-vUZCTFX4vsSmMIY {\n  height: 0;\n  padding: 0;\n}\n", ""]);
 	
 	// exports
 	exports.locals = {
