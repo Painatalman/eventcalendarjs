d35c409e88f11d111d927dc95964280d39eff100
Painatalman <maiquedi@gmail.com>
2017-01-20 20:32:37 +0000

    wip method for fetching events

--- src/components/EventCollection.js
+++ src/components/EventCollection.js
@@ -108,4 +108,27 @@ export default class EventCollection {
 
     return singleEvents.concat(yearlyEvents);
   }
+
+  fetchEventsFromGoogleCalendar(calendarId, apiKey, baseUrl='https://www.googleapis.com/calendar/v3/calendars') {
+    let url = `${baseUrl}/${calendarId}/${apiKey}`;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var resp = request.responseText;
+      } else {
+        // We reached our target server, but it returned an error
+
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    request.send();
+  }
 }
